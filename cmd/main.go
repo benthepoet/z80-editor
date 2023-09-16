ebc27a5e9833f10f8a3db210b8647c7f5e11ca67
Ben Hanna <benpaulhanna@gmail.com>
2023-09-16 13:59:06 -0400

    More compaction.

--- cmd/main.go
+++ cmd/main.go
@@ -5,9 +5,11 @@ import (
 	"os"
 )
 
+const missing_file = "Filename was not supplied"
+
 func main() {
 	if len(os.Args) < 1 {
-		panic("Must supply a file name")
+		panic(missing_file)
 	}
 
 	f, err := os.Open(os.Args[1])
@@ -37,41 +39,38 @@ func main() {
 	}
 }
 
-func getInstrLen(i byte) byte {
-	if i < 0x40 {
+func getInstrLen(op byte) byte {
+	if op < 0x40 {
 		// Upper table
-		l := i & 0x0f
+		lnb := op & 0x0f
 
-		if i == 0x00 {
+		if op == 0x00 {
 			return 1
-		} else if l == 0x00 || l == 0x06 || l == 0x0e {
+		} else if lnb == 0x00 || lnb == 0x06 || lnb == 0x0e {
 			return 2
-		} else if l == 0x01 || i == 0x22 || i == 0x32 || i == 0x2a || i == 0x3a {
+		} else if lnb == 0x01 || op == 0x22 || op == 0x32 || op == 0x2a || op == 0x3a {
 			return 3
-		} else {
-			return 1
 		}
-	} else if i < 0xc0 {
+		return 1
+	} else if op < 0xc0 {
 		// Middle table
 		return 1
-	} else if i == 0xcb {
+	} else if op == 0xcb {
 		return 0
-	} else if i == 0xdd {
+	} else if op == 0xdd {
 		return 0
-	} else if i == 0xed {
+	} else if op == 0xed {
 		return 0
-	} else if i == 0xfd {
+	} else if op == 0xfd {
 		return 0
-	} else {
-		// Bottom table
-		l := i & 0x0f
-
-		if l == 0x02 || l == 0x04 || l == 0x0a || l == 0x0c || i == 0xc3 || i == 0xcd {
-			return 3
-		} else if l == 0x06 || l == 0x0e || i == 0xd3 || i == 0xdb {
-			return 2
-		} else {
-			return 1
-		}
 	}
+	// Bottom table
+	lnb := op & 0x0f
+
+	if lnb == 0x02 || lnb == 0x04 || lnb == 0x0a || lnb == 0x0c || op == 0xc3 || op == 0xcd {
+		return 3
+	} else if lnb == 0x06 || lnb == 0x0e || op == 0xd3 || op == 0xdb {
+		return 2
+	}
+	return 1
 }
